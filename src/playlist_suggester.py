09259a57b8e5c7c9499ae488f42110367cbfb48f
AdamGolota <golota.adam@gmail.com>
2021-09-12 14:14:01 +0300

    cheat fix

--- src/playlist_suggester.py
+++ src/playlist_suggester.py
@@ -1,9 +1,9 @@
 class CustomPlaylistSuggester:
     def suggest_playlist(self, emotion):
         emotion_to_playlist = {
-            'Angry': '0.1.1',
+            'Angry': '0.0.0',
             'Disgust': '0.4.0',
-            'Fear': '0.10.0',
+            'Fear': '0.0.1',
             'Happy': '0.0.1',
             'Sad': '0.3.0',
             'Surprise': '0.8.0',
