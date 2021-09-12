bd28ba9c0ae167703c022f44c4c3d5582d21dba0
AdamGolota <golota.adam@gmail.com>
2021-09-12 14:51:02 +0300

    do fine-grained emotion playlists

--- src/playlist_suggester.py
+++ src/playlist_suggester.py
@@ -1,12 +1,12 @@
 class CustomPlaylistSuggester:
     def suggest_playlist(self, emotion):
         emotion_to_playlist = {
-            'Angry': '0.0.0',
-            'Disgust': '0.4.0',
-            'Fear': '0.0.1',
-            'Happy': '0.0.1',
+            'Angry': '6.17.2',
+            'Disgust': '0.10.0',
+            'Fear': '0.8.0',
+            'Happy': '0.11.0',
             'Sad': '0.3.0',
-            'Surprise': '0.8.0',
-            'Neutral': '2.0.0'
+            'Surprise': '6.6.4',
+            'Neutral': '2.2.0'
         }
         return emotion_to_playlist[emotion]
